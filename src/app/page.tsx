2284d643a25cd886a9460d27b9b08df2ce273dee
Rozycki, Bartosz <bartek.rozycki@icloud.com>
2024-10-18 13:20:16 +0200

    template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,73 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    return (
+        <main className={"main-content"}>
+            <h2>Die wichtigsten Aktivitäten im Überblick</h2>
+            <div className="section">
+                <div className="section-item">
+                    <h3>12 neue Aufträge zur Freigabe</h3>
+                    <p>GPP</p>
+                    <div className="top-right">NEU</div>
+                    <button>Jetzt freigeben</button>
+                </div>
+                <div className="section-item">
+                    <h3>3 neue Aufträge zur Freigabe</h3>
+                    <p>Auftragsübersicht</p>
+                    <div className="top-right">NEU</div>
+                    <button>Jetzt freigeben</button>
+                </div>
+                <div className="section-item">
+                    <h3>0 Nachrichten</h3>
+                    <p>Postbox</p>
+                    <button>Zur Postbox</button>
+                </div>
+            </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+            <div className="overview">
+                <div className="overview-item">
+                    <h3>Gesamtsaldo aller Produkte: 12.345.678,90 €</h3>
+                    <div className="circle-chart"></div>
+                </div>
+                <div className="overview-item">
+                    <h3>Meist genutzte Konten</h3>
+                    <ul className="account-list">
+                        <li>
+                            Premium Geschäftskonto<br/><span
+                        >DE99 5004 0000 1122 3344 55</span
+                        >
+                            +124.593,00 €
+                        </li>
+                        <li>
+                            Premium Geschäftskonto<br/><span
+                        >DE99 5004 0000 1122 3344 55</span
+                        >
+                            -1.500,00 €
+                        </li>
+                        <li>
+                            Premium Geschäftskonto<br/><span
+                        >DE99 5004 0000 1122 3344 55</span
+                        >
+                            +74.482,00 €
+                        </li>
+                        <li>
+                            Premium Geschäftskonto<br/><span
+                        >DE99 5004 0000 1122 3344 55</span
+                        >
+                            +163.500,00 €
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="transactions">
+                <div className="transaction-item">
+                    <p>Ihr Auftrag 123XY wurde freigegeben</p>
+                    <small>GPP | 28.09.2023 | 13:45 Uhr</small>
+                </div>
+                <div className="transaction-item">
+                    <p>Ihr Auftrag XYZ123 wurde freigegeben</p>
+                    <small>GPP | 28.09.2023 | 11:37 Uhr</small>
+                </div>
+            </div>
+        </main>
+    );
 }
