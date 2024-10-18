6fe8e417af10228ae931870b68a2b52b80dc8ba5
krzysztofwiedenski <krzysztofwiedenski@gmail.com>
2024-10-18 20:21:21 +0200

    repair logo

--- src/app/Logo.tsx
+++ src/app/Logo.tsx
@@ -1,14 +1,8 @@
-'use client';
+"use client";
 
 import Image from "next/image";
 import logo from "./logo.svg";
 
 export const Logo = () => {
-    return (
-        <Image
-            className={"max-w-64"}
-            src={logo}
-            alt="Commerzbank Logo"
-        />
-    )
-}
+  return <Image className={"max-w-56 "} src={logo} alt="Commerzbank Logo" />;
+};
