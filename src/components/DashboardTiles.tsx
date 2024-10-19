9216594b38442164dd7cd89162f2fcfd3fe97097
Rozycki, Bartosz <bartek.rozycki@icloud.com>
2024-10-19 13:21:23 +0200

    score indicator layer

--- src/components/DashboardTiles.tsx
+++ src/components/DashboardTiles.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {ScoreIndicator} from "@/components/ScoreIndicator";
 
 interface CurrencyData {
     currency: string;
@@ -11,9 +12,9 @@ interface DashboardTilesProps {
     accounts: CurrencyData[];
 }
 
-const DashboardTiles: React.FC<DashboardTilesProps> = ({ accounts }) => {
+const DashboardTiles: React.FC<DashboardTilesProps> = ({accounts}) => {
     return (
-        <div className="grid grid-cols-1 gap-4 p-4 lg:grid-cols-1 md:grid-cols-1 sm:grid-cols-1">
+        <div className="grid grid-cols-1 gap-4 p-4 px-6 lg:grid-cols-1 md:grid-cols-1 sm:grid-cols-1">
             {accounts.map((account, index) => {
                 // Calculate the change
                 const change = account.rate - account.previousRate;
@@ -22,78 +23,85 @@ const DashboardTiles: React.FC<DashboardTilesProps> = ({ accounts }) => {
                 return (
                     <div
                         key={index}
-                        className="bg-white px-8 py-2 rounded-lg  transition-shadow relative"
-                        style={{ width: '100%', height: '100%' }}
+                        className="bg-white px-0 py-2  transition-shadow relative flex border-b-[1px]"
+                        style={{width: '100%', height: '100%'}}
                     >
-                        <div className="flex items-center justify-between mb-2">
-                            <div className="text-2xl font-bold text-gray-700 flex gap-4">
-                                EUR/{account.currency}
-
-                                <span className="bg-green-100 text-green-600 font-semibold py-1 px-3 rounded-full text-xs">
-                                    Buy
-                                </span>
-                            </div>
-                            {/* Notification Bell */}
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth={1.5}
-                                stroke="currentColor"
-                                className="size-6"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5"
-                                />
-                            </svg>
+                        <div className="px-2 py-6">
+                            <ScoreIndicator value={5}/>
                         </div>
 
-                        <div className="flex items-center mb-4">
-                            {/* Current rate */}
-                            <span className="text-xl font-bold text-black">{account.rate.toFixed(4)}</span>
+                        <div className={"ml-4"}>
+                            <div className="flex items-center justify-between mb-2">
 
-                            {/* Change in value */}
-                            <span
-                                className={`ml-2 text-xs font-semibold ${
-                                    change > 0 ? 'text-green-600' : 'text-red-600'
-                                }`}
-                            >
+                                <div className="text-2xl font-bold text-gray-700 flex gap-4">
+                                    EUR/{account.currency}
+
+                                    {/*<span className="bg-green-100 text-green-600 font-semibold py-1 px-3 rounded-full text-xs">*/}
+                                    {/*    Buy*/}
+                                    {/*</span>*/}
+                                </div>
+                                {/* Notification Bell */}
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="size-6"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5"
+                                    />
+                                </svg>
+                            </div>
+
+                            <div className="flex items-center mb-4">
+                                {/* Current rate */}
+                                <span className="text-xl font-bold text-black">{account.rate.toFixed(4)}</span>
+
+                                {/* Change in value */}
+                                <span
+                                    className={`ml-2 text-xs font-semibold ${
+                                        change > 0 ? 'text-green-600' : 'text-red-600'
+                                    }`}
+                                >
                                 {change > 0 ? '+' : ''}
-                                {change.toFixed(4)} ({percentageChange.toFixed(2)}%)
+                                    {change.toFixed(4)} ({percentageChange.toFixed(2)}%)
                             </span>
 
-                            {/* Up/Down arrow */}
-                            {change > 0 ? (
-                                <span className="ml-1 text-green-600">▲</span>
-                            ) : (
-                                <span className="ml-1 text-red-600">▼</span>
-                            )}
-                        </div>
+                                {/* Up/Down arrow */}
+                                {change > 0 ? (
+                                    <span className="ml-1 text-green-600">▲</span>
+                                ) : (
+                                    <span className="ml-1 text-red-600">▼</span>
+                                )}
+                            </div>
 
-                        <div className=" grid grid-cols-6 gap-2 justify-end -mt-8">
-                            <div className="col-span-3"></div>
-                            <div
-                                className="flex items-center justify-center gap-2 text-gray-900 font-semibold text-md px-6 py-3 rounded-full w-full cursor-pointer transition-transform transform hover:scale-105 "
-                            >
-                                Loan
-                                <span className="ml-2">→</span>
-                            </div>
-                            <div
-                                className="flex items-center justify-center gap-2 text-gray-900 font-semibold text-md px-6 py-3 rounded-full w-full cursor-pointer transition-transform transform hover:scale-105  "
-                            >
-                                Exchange
-                                <span className="ml-2">→</span>
-                            </div>
-                            <div
-                                className="flex items-center justify-center gap-2 text-gray-900 font-semibold text-md px-6 py-3 rounded-full w-full cursor-pointer transition-transform transform hover:scale-105 "
-                            >
-                                Forex
-                                <span className="ml-2">→</span>
+                            <div className=" grid grid-cols-6 gap-2 justify-end -mt-8">
+                                <div className="col-span-3"></div>
+                                <div
+                                    className="flex items-center justify-center gap-2 text-gray-900 font-semibold text-md px-6 py-3 rounded-full w-full cursor-pointer transition-transform transform hover:scale-105 "
+                                >
+                                    Loan
+                                    <span className="ml-2">→</span>
+                                </div>
+                                <div
+                                    className="flex items-center justify-center gap-2 text-gray-900 font-semibold text-md px-6 py-3 rounded-full w-full cursor-pointer transition-transform transform hover:scale-105  "
+                                >
+                                    Exchange
+                                    <span className="ml-2">→</span>
+                                </div>
+                                <div
+                                    className="flex items-center justify-center gap-2 text-gray-900 font-semibold text-md px-6 py-3 rounded-full w-full cursor-pointer transition-transform transform hover:scale-105 "
+                                >
+                                    Forex
+                                    <span className="ml-2">→</span>
+                                </div>
                             </div>
                         </div>
-                         <hr className={'mt-4'}/>
+                        <hr/>
                     </div>
                 );
             })}
