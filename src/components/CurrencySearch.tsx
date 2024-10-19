b20335959d2e3ac1e4779e5edfcc43c156bfa4d2
Rozycki, Bartosz <bartek.rozycki@icloud.com>
2024-10-19 15:08:41 +0200

    remove <b>

--- src/components/CurrencySearch.tsx
+++ src/components/CurrencySearch.tsx
@@ -52,7 +52,7 @@ const CurrencySearch = () => {
                     disabled={true}
                     className="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
                 <option value="EUR" selected>
-                    <b>EUR</b>
+                    EUR
                 </option>
             </select>
 
